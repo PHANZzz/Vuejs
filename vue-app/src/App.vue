966985cc6536533c39add58b02a57554841e5a35
Kan Sophan <ytbro65@gmail.com>
2023-07-05 07:29:25 +0700

    added

---
+++ vue-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <br>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/icons/Home.vue'
+
+export default {
+  components: {
+    Home
+  },
+  data() {
+    return {
+      message: 'Hello'
+    }
+  }
+}
+</script>
